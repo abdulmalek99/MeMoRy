9a848b23f0f14a1b8b5553fab187413ed4232c5a
Abdulmalek Muthana Abdulmalek <9905abab@res.ludvika.intra>
2018-10-03 10:55:32 +0200

    Lagt till event-lyssnare

--- src/js/memory.js
+++ src/js/memory.js
@@ -1,14 +1,11 @@
-// TODO: 1.3. varje bricka behöver en bild på dess framsida
-// TODO: 1.4. Skriva ut 4x4 brickor
-// TODO: 1.5. Brickorna bör ligga i ett rutnät på 4 rader, 4 brickor per rad
-// TODO: 1.6. Man ksa kunna vända en bricka genom att trycka på den.
-// TODO: 1.7. Om 2 olika brickor vänds så ska dom tas bort.
-// TODO: 1.8. om 2 olika brickor vänds så ska dem vändas tillbaka.
-// TODO: 1.9. När 2 brickor vänds ska antalet försök uppdateras
-// TODO: 1.10. När 2 lika brickor vänds ska antalet par uppdateras
-// TODO: 1.11. Antalet försök och antalet par ska visas för användaren.
-// TODO: 1.12. Spelaren ska kunna se under hur mång sekunder som han har spelat.
-// TODO: 1.13. När spelet är slut ska antaöet sekunder slut räknas
+// TODO: 1.3. Om 2 olika brickor vänds så ska dom tas bort.
+// TODO: 1.4. om 2 olika brickor vänds så ska dem vändas tillbaka.
+// TODO: 1.5. När 2 brickor vänds ska antalet försök uppdateras
+// TODO: 1.6. När 2 lika brickor vänds ska antalet par uppdateras
+// TODO: 1.7. Brickorna måste blandas
+// TODO: 1.8. Antalet försök och antalet par ska visas för användaren.
+// TODO: 1.9. Spelaren ska kunna se under hur mång sekunder som han har spelat.
+// TODO: 1.10. När spelet är slut ska antalet sekunder slut räknas
 // TODO: 2. Det ska enkelt gå att ladda in flera spel, genom att anropa en funktion flera gånger.
 /* TODO: 3. En enklare dokumenatation i README.md som ska vara skriven i markup språket Markdown.
 Bör innhållar kortare information om vad som ligger i respektive fil samt vilka kommandon som ska
@@ -16,6 +13,8 @@ köras för att starta utvecklingsserver samt hur man bygger en build. */
 const memory = () => {
   const rows = 4; // Antalet rader
   const columns = 4; // Antalet kolumner
+
+  const tiles = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 
   const containerId = 'memory';
 
@@ -29,8 +28,28 @@ const memory = () => {
 
   container.appendChild(div);
 
-  for (let i = 0; i < rows * columns; i++) {
+// FIXME:
+
+  for (let i = 0; i < tiles.length; i++) {
+// FIXME: 
+
+    const handleClick = event => {
+      // FIXME:
+      let img;
+      if (event.target.tagName === 'DIV') {
+        img = event.target.firstElementChild;
+      } else {
+        img = event.target;
+      }
+      const path = `images/${tiles[i]}.png`;
+      img.setAttribute('src', path);
+    };
+
+
+
     const brick = document.importNode(templateDiv.firstElementChild, true);
+    // FIXME:
+    brick.addEventListener('click', handleClick);
     div.appendChild(brick);
   }
 };
