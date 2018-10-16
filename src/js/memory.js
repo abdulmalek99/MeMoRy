d02ecb9d77d65e313d991cfcf0f206288e87e609
Abdulmalek Muthana Abdulmalek <9905abab@res.ludvika.intra>
2018-10-16 15:06:40 +0200

    lLöst bugg (close #1)

--- src/js/memory.js
+++ src/js/memory.js
@@ -21,7 +21,7 @@ const turnBrick = (bricks, img) => {
   } else {
     bricks.second = img;
 
-    if (bricks.first.getAttribute('src') === bricks.second.getAttribute('src')) {
+    if (bricks.first.getAttribute('src') === bricks.second.getAttribute('src') && bricks.first.getAttribute('data-index-number') !== bricks.second.getAttribute('data-index-number')) {
       // FIXME:
 
       const removeBrick = () => {
@@ -84,6 +84,7 @@ const renderMemory = (containerId, bricks) => {
     // FIXME:
 
     brick.addEventListener('click', handleClick);
+    brick.firstElementChild.setAttribute('data-index-number', i);
     div.appendChild(brick);
   }
 };
